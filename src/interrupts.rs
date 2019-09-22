878dd1c542a0ea212f8f0187beb3bee86308ef39
Philipp Oppermann <dev@phil-opp.com>
2019-09-22 16:56:20 +0200

    Create a `UsableOnce` type for reducing stack usage when initializing IDT

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -33,24 +33,47 @@ impl InterruptIndex {
 pub static PICS: spin::Mutex<ChainedPics> =
     spin::Mutex::new(unsafe { ChainedPics::new(PIC_1_OFFSET, PIC_2_OFFSET) });
 
-lazy_static! {
-    static ref IDT: InterruptDescriptorTable = {
-        let mut idt = InterruptDescriptorTable::new();
-        idt.breakpoint.set_handler_fn(breakpoint_handler);
-        idt.page_fault.set_handler_fn(page_fault_handler);
-        unsafe {
-            idt.double_fault
-                .set_handler_fn(double_fault_handler)
-                .set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX);
+
+use core::cell::UnsafeCell;
+use core::sync::atomic::{AtomicBool, Ordering};
+struct UsableOnce<T> {
+    inner: UnsafeCell<T>,
+    called: AtomicBool,
+}
+unsafe impl<T> Sync for UsableOnce<T> {}
+impl<T> UsableOnce<T> {
+    const fn new(value: T) -> Self {
+        UsableOnce {
+            inner: UnsafeCell::new(value),
+            called: AtomicBool::new(false),
         }
-        idt[InterruptIndex::Timer.as_usize()].set_handler_fn(timer_interrupt_handler);
-        idt[InterruptIndex::Keyboard.as_usize()].set_handler_fn(keyboard_interrupt_handler);
-        idt
-    };
+    }
+
+    fn get_mut(&self) -> Result<&mut T, ()> {
+        let called = self.called.swap(true, Ordering::SeqCst);
+        if called {
+            Err(())
+        } else {
+            Ok(unsafe { &mut *self.inner.get() })
+        }
+    }
 }
 
+static IDT: UsableOnce<InterruptDescriptorTable> = UsableOnce::new(InterruptDescriptorTable::new());
+
 pub fn init_idt() {
-    IDT.load();
+    let idt = IDT.get_mut().expect("init_idt called twice");
+    idt.breakpoint.set_handler_fn(breakpoint_handler);
+    idt.page_fault.set_handler_fn(page_fault_handler);
+    unsafe {
+        idt.double_fault
+            .set_handler_fn(double_fault_handler)
+            .set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX);
+    }
+    idt[InterruptIndex::Timer.as_usize()].set_handler_fn(timer_interrupt_handler);
+    idt[InterruptIndex::Keyboard.as_usize()].set_handler_fn(keyboard_interrupt_handler);
+
+    idt.load();
 }
 
 extern "x86-interrupt" fn breakpoint_handler(stack_frame: &mut InterruptStackFrame) {
